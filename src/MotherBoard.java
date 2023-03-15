56a57050d931abc32a90d6683b2113ae3a7950b7
Artyomka P <artyompionov@gmail.com>
2023-03-15 10:02:17 +0000

    Minor code formatting changes.

--- src/MotherBoard.java
+++ src/MotherBoard.java
@@ -7,15 +7,16 @@ public class MotherBoard {
         int usb2;
         int usb3;
 
-        int getTotalPorts(){
+        int getTotalPorts() {
             return usb2 + usb3;
         }
 
-        USB(int usb2, int usb3){
+        USB(int usb2, int usb3) {
             this.usb2 = usb2;
             this.usb3 = usb3;
         }
     }
+
     USB usb = new USB(2, 3); // Example Constructor
 }
 
