c1e84fbd0520618f1857770b4cc42e1a3a39ce6b
Artyomka P <artyompionov@gmail.com>
2023-03-15 10:35:00 +0000

    Better Code for the TVset.java

--- src/TVset.java
+++ src/TVset.java
@@ -1,49 +1,46 @@
 // Example code used to learn about Nested and Inner classes.
 class TVset {
     //---Fields---
-    int channel = 5;
-    boolean isOn;
+    private int channel = 1;
+    private boolean isOn;
+    private Remote remote = new Remote(); // Creating remote object.
+
+    // Remote getter
+    public Remote getRemote() {
+        return remote; // Returns private remote object
+    }
+
+    // Inner class Remote
+    class Remote {
+        //---Setters---
+        public void setChannel(int channel) {
+            TVset.this.channel = channel;
+        }
+
+        public void turnOn() {
+            isOn = true;
+        }
+
+        public void turnOff() {
+            isOn = false;
+        }
+    }
 
     // Example of Static Polymorphism
     @Override
-    public String toString() {
-        if (!isOn) {
-            return "The TVset is OFF";
+    public String toString(){
+        if (!isOn){
+            return "TV is off.";
         }
-        return "Channel " + channel + " is on the TVset now.";
-    }
-}
-
-// Nested class
-class Remote {
-    // Reference to the TVset class so the remote "knows" what to control.
-    private TVset tvSet;
-
-    public void setTVset(TVset tvSet) {
-        this.tvSet = tvSet;
-    }
-
-    public void setChannel(int channel) {
-        tvSet.channel = channel;
-    }
-
-    public void turnOn() {
-        tvSet.isOn = true;
-    }
-
-    public void turnOff() {
-        tvSet.isOn = false;
+        return "Channel " + channel + " is on.";
     }
 }
 
 // Class to Run the code...
 class TestRun {
     public static void main(String[] args) {
-
         TVset tv = new TVset();
-        Remote remote = new Remote();
-
-        remote.setTVset(tv);
+        TVset.Remote remote = tv.getRemote();
         System.out.println(tv);
         remote.turnOn();
         System.out.println(tv);
